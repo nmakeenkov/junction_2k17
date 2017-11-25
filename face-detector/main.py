b45c9067f145c7e40b30af1283fefac8b05b0dbc
nmakeenkov <n.makeenkov@yandex.ru>
2017-11-26 01:54:54 +0300

    Use dlib face detection

--- face-detector/main.py
+++ face-detector/main.py
@@ -1,10 +1,15 @@
 
 # coding: utf-8
+import datetime
 
 import cv2
 import os
+
+import dlib
 import numpy as np
 import uuid
+
+import skimage
 
 import data_manager
 import util
@@ -31,41 +36,50 @@ def predict(face_recognizer, test_img):
 
 if __name__ == '__main__':
     data_manager.DataManager.get_persons()
-    faces, labels = data_manager.DataManager.get_data()
+    vecs, labels = data_manager.DataManager.get_data()
 
-    print("Total faces: ", len(faces))
-    print("Total labels: ", len(labels))
-
-
-    # face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-    # face_recognizer = cv2.face.EigenFaceRecognizer_create()
-    face_recognizer = cv2.face.FisherFaceRecognizer_create()
-
-    face_recognizer.train(faces, np.array(labels))
-
-    cv2.namedWindow("person")
     vc = cv2.VideoCapture(0)
 
     rval = vc.isOpened()
+
+    im_cnt = 0
+    save_faces = False
+    win = dlib.image_window()
+    person = data_manager._Person(-1)  # fake person
+    current_text = None
+    fails_cnt = 0
     while rval:
         rval, frame = vc.read()
-        predicted_img1 = predict(face_recognizer, frame)
-        if predicted_img1 is not None:
-            cv2.imshow("person", predicted_img1)
+        image = frame[:, :, ::-1]
+        if current_text is not None:
+            util.draw_text(frame, current_text, 210, 40)
+            win.set_image(frame[:, :, ::-1])
         else:
-            cv2.imshow("person", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyWindow("person")
+            win.set_image(image)
 
-    for image__ in os.listdir('test-data'):
-        test_img = cv2.imread("test-data/{}".format(image__))
-
-        print(image__)
-        predicted_img1 = predict(face_recognizer, test_img)
-
-        if predicted_img1 is not None:
-            cv2.imshow(str(uuid.uuid4()), predicted_img1)
+        win.clear_overlay()
+        try:
+            person.detect_face(image, win)
+        except data_manager.NoFaces:
+            fails_cnt += 1
+            if fails_cnt > 3:
+                current_text = None
+        else:
+            fails_cnt = 0
+            vec = person.vecs[-1]
+            best_dist = None
+            best_idx = -1
+            for idx, v in enumerate(vecs):
+                dist = np.linalg.norm(vec - v)
+                if best_dist is None or dist < best_dist:
+                    best_dist = dist
+                    best_idx = idx
+            if best_dist > .5:
+                current_text = 'STRANGER'
+            else:
+                p = data_manager.DataManager.get_person(labels[best_idx])
+                current_text = 'Person {}: {}'.format(p.info['id'], p.info['data'].get('name', 'Unknown'))
+                print(best_dist)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
